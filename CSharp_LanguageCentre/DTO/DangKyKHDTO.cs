ccf5f9f545101ae90487fc90d4738503b27b63d0
Thien Huong <thienhuong6935@gmail.com>
2023-10-24 21:47:24 +0700

    Them KhoaHocDTO, DangKyKHDTO

---
+++ CSharp_LanguageCentre/DTO/DangKyKHDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_LanguageCentre.DTO
+{
+    class DangKyKHDTO
+    {
+        int maKH;
+        int maHV;
+        float diemGK;
+        float diemCK;
+
+        public DangKyKHDTO(int maKH, int maHV, float diemGK, float diemCK)
+        {
+            this.maKH = maKH;
+            this.maHV = maHV;
+            this.diemGK = diemGK;
+            this.diemCK = diemCK;
+        }
+
+        public int MaKH { get; set; }
+        public int MaHV { get; set; }
+        public float DiemGK { get; set; }
+        public float DiemCK { get; set; }
+    }
+}
---
+++ CSharp_LanguageCentre/DTO/KhoaHocDTO.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_LanguageCentre.DTO
+{
+    class KhoaHocDTO
+    {
+        int maKH;
+        string tenKH;
+        int gia;
+        string capBac;
+        DateTime ngayBD;
+        DateTime ngayKT;
+
+        public KhoaHocDTO(int maKH, string tenKH, int gia, string capBac, DateTime ngayBD, DateTime ngayKT)
+        {
+            this.maKH = maKH;
+            this.tenKH = tenKH;
+            this.gia = gia;
+            this.capBac = capBac;
+            this.ngayBD = ngayBD;
+            this.ngayKT = ngayKT;
+        }
+        
+        public int MaKH { get; set; }
+        public string TenKH { get; set; }
+        public int Gia { get; set; }
+        public string CapBac { get; set; }
+        public DateTime NgayBD{ get; set; }
+        public DateTime NgayKT{ get; set; }
+    }
+}
